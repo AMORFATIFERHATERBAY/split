5721636f6aee3ffde3be65c30d7990140a1b6a60
AmorFatiBA <you@example.com>
2020-09-14 18:39:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "ahmet,mehmet,orhan,sami";
+            string[] strDizi = str.Split(",");
+            for (int i = 0; i < strDizi.Length; i++)
+                Console.WriteLine(strDizi[i]);
+        }
+    }
+}

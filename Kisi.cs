1eea43b5ab02bc4786296a537deeab5b8d186d21
AmorFatiBA <you@example.com>
2020-09-14 19:10:44 +0300

    mod

---
+++ Kisi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace split
+{
+    public class Kisi
+    {
+        private string adi;
+        private string soyadi;
+        private string tel;
+        private string mail;
+
+        public string Adi {get; set;}
+        public string Soyadi {get; set;}
+        public string Tel {get; set;}
+        public string Mail {get; set;}
+       
+}
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,27 @@ namespace split
     {
         static void Main(string[] args)
         {
-            string str = "ahmet,mehmet,orhan,sami";
+            string str = "Ahmet,Bayat,05363662323,a.bayat@hotmail.com";
             string[] strDizi = str.Split(",");
+
             for (int i = 0; i < strDizi.Length; i++)
                 Console.WriteLine(strDizi[i]);
+
+            Kisi kisi = new Kisi();
+
+            for (int i = 0; i < strDizi.Length; i++)
+            {
+                kisi.Adi = strDizi[0];
+                kisi.Soyadi = strDizi[1];
+                kisi.Tel = strDizi[2];
+                kisi.Mail = strDizi[3];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Adi    :" + kisi.Adi);
+            Console.WriteLine("Soyadi :" + kisi.Soyadi);
+            Console.WriteLine("Tel    :" + kisi.Tel);
+            Console.WriteLine("Mail   :" + kisi.Mail);
+
         }
     }
 }
